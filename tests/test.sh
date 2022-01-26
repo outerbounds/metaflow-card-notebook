734a95f7cdc747152ac2bf7e1a3398a2f1f696f5
hamelsmu <hamel.husain@gmail.com>
2022-01-25 18:51:16 -0800

    add tests

--- tests/test.sh
+++ tests/test.sh
@@ -3,7 +3,7 @@
 set -eu
 RESULTS=$(python nbflow.py card get end)
 if [[ "$RESULTS" = *"ERROR"* ]]; then
-    echo "Test Failed.\nGot:\n$Results\nRender results in your browser to debug with the command: make render$1"
+    echo "Test Failed. Got:\n$RESULTS\nRender results in your browser to debug with the command: make render$1"
     exit 1
 else
     echo "Test Passed"
